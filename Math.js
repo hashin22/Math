d1b538615c38cbfed1d11a683bbbfb9579280631
haran <hashin22@naver.com>
2020-12-19 23:45:10 +0900

    math.random함수사용

--- Math.js
+++ Math.js
@@ -1,16 +1,13 @@
-function testMath() {
+function getRandom() {
     //Math.random() 함수사용해보기
-    let num;
-    for (let i = 0; i < 30; i++) {
-        num = Math.random() * 100;
-        num = Math.ceil(num) * 1000;
-
-        document.querySelector("p").innerHTML += num + "<br>";
-    }
+    let num = Math.random() * 100;
+    num = Math.ceil(num) * 1000;
+    return num;
 }
 
 
+
 window.onload = function() {
-    testMath();
+    document.querySelector("#amount").value = getRandom();
 
 }
